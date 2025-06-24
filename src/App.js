924a5afb233e99338a12a7de96a427f20223dd7d
Brad Alexander <bradalexander@Brads-MacBook-Air.local>
2025-06-24 15:17:50 -0500

    reverting to section 3 code

--- src/App.js
+++ src/App.js
@@ -2,61 +2,46 @@ import "./App.css";
 import EnrolmentForm from "./EnrolmentForm";
 import { useState } from "react";
 
-const App = () => {
+function App() {
   const [program, setProgram] = useState("UG");
   const [ugSeats, setUgSeats] = useState(60);
   const [pgSeats, setPgSeats] = useState(40);
 
   const handleChange = (event) => {
     setProgram(event.target.value);
-    setPgSeats(pgSeats);
-    setUgSeats(ugSeats);
   };
-const setUpdatedSeats = (updatedSeats) => {
+  const setUpdatedSeats = (updatedSeats) => {
     if (program === "UG") {
       setUgSeats(updatedSeats);
     } else {
       setPgSeats(updatedSeats);
     }
   };
-
   return (
     <div className="App">
       <div className="programs">
-      <h3 className="title">Student Enrolment Form</h3>
-        <ul className="ulEnrol">
-
-  <li className="parentLabels" onChange={handleChange}>
-            <input
-              type="radio"
-              value="UG"
-              name="programGroup"
-              defaultChecked
-            />
-            Undergraduate
-            <input
-              type="radio"
-              className="radioSel"
-              value="PG"
-              name="programGroup"
-            />
-            Postgraduate
-          </li>
-          <li>
-            <label className="parentLabels">
-              Remaining {program} Seats - {program === "UG" ? ugSeats : pgSeats}
-            </label>
-          </li>
-        </ul>
+        <label>Remaining UG Seats - {ugSeats}</label>
+        <br />
+        <br />
+        <label>Remaining PG Seats - {pgSeats}</label>
+        <br />
+        <br />
+        <label>Choose Program:</label>
+        <select
+          className="appDropDowns"
+          onChange={handleChange}
+          value={program}
+        >
+          <option value="UG">Undergraduate</option>
+          <option value="PG">Postgraduate</option>
+        </select>
       </div>
       <EnrolmentForm
         chosenProgram={program}
-        setUpdatedSeats={setUpdatedSeats}
-
+        setUpdatedSeats={ setUpdatedSeats}
         currentSeats={program === "UG" ? ugSeats : pgSeats}
       />
     </div>
   );
 }
-
 export default App;
